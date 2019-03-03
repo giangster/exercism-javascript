4be641fb8b8d435b5dd10806bd2c88d03f2c1786
Giang Nguyen <thuygiang152@gmail.com>
2019-03-03 20:23:30 +0200

    some changes for the leap exercises, final ver

--- leap/leap.js
+++ leap/leap.js
@@ -1,18 +1,22 @@
-export function isLeap(year) {
-    if (year % 4 == 0) {
-        if (year % 100 == 0 && year % 400 !== 0) {
-            return false;
-        } else if (year % 400 == 0) {
-            return true;
-        } else {
-            return true;
-        }
-    } else {
-        return false;
-    }
-}
+// export function isLeap(year) {
+//     if (year % 4 == 0) {
+//         if (year % 100 == 0 && year % 400 !== 0) {
+//             return false;
+//         } else if (year % 400 == 0) {
+//             return true;
+//         } else {
+//             return true;
+//         }
+//     } else {
+//         return false;
+//     }
+// }
 
 // function isLeap(year)
 // {
 // return (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);
 // }
+
+export function isLeap(year) {
+    return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0) ? true : false;
+}
