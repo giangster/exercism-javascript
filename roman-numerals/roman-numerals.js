1c764abadd5bea415c8e89f3798688095d7d4b04
Giang Nguyen <thuygiang152@gmail.com>
2019-03-16 17:29:30 +0200

    roman numerals

---
+++ roman-numerals/roman-numerals.js
@@ -0,0 +1,42 @@
+export function toRoman(number) {
+  let oneToTen = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"];
+  let tenToHundred = [
+    "",
+    "X",
+    "XX",
+    "XXX",
+    "XL",
+    "L",
+    "LX",
+    "LXX",
+    "LXXX",
+    "XC",
+    "C"
+  ];
+  let hundredToThousand = [
+    "",
+    "C",
+    "CC",
+    "CCC",
+    "CD",
+    "D",
+    "DC",
+    "DCC",
+    "DCCC",
+    "CM",
+    "M"
+  ];
+  let roman = "";
+  roman = roman + hundredToThousand[10].repeat(Math.floor(number / 1000));
+  number = number % 1000;
+  roman = roman + hundredToThousand[Math.floor(number / 100)];
+  number = number % 100;
+  roman = roman + tenToHundred[Math.floor(number / 10)];
+  number = number % 10;
+  if (number > 0) {
+    roman = roman + oneToTen[number - 1];
+    return roman;
+  } else {
+    return roman;
+  }
+}
