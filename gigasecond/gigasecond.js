eac9457f184456de528d63fcdd197878a0dd91dc
Giang Nguyen <thuygiang152@gmail.com>
2019-03-27 15:03:18 +0200

    improved ver

--- gigasecond/gigasecond.js
+++ gigasecond/gigasecond.js
@@ -1,8 +1,3 @@
-// function parseISOString(s) {
-//   var b = s.split(/\D+/);
-//   return new Date(Date.UTC(b[0], --b[1], b[2], b[3], b[4], b[5], b[6]));
-// }
-
 export function gigasecond(date) {
   return new Date(new Date(date).getTime() + Math.pow(10, 12));
 }
