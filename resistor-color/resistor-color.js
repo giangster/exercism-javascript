039b48b9376ccc11f3ae3e42e0a74ec779781741
Giang Nguyen <thuygiang152@gmail.com>
2019-10-17 22:05:16 +0300

    Improved solution for Resistor color

--- resistor-color/resistor-color.js
+++ resistor-color/resistor-color.js
@@ -4,8 +4,7 @@
 //
 
 export const colorCode = string => {
-  let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  return numbers[COLORS.indexOf(string)];
+  return COLORS.indexOf(string);
 };
 export const COLORS = [
   "black",
